cff50f8e158410632136aaa3e43d4a91c1377d45
heyifeng226 <46611375+heyifeng226@users.noreply.github.com>
2020-12-04 02:01:39 -0800

    update homepage

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Form from "../Form/Form";
-import background from "../images/background.jpg";
 import styles from "./HomePage.module.css";
 
 const Home = () => {
     return (
         <div className={styles.homeContainer}>
-      <img
+    <img
         className={styles.homeImg}
-        srcset="/images/homekitchen-400.png 400w, /images/homekitchen-600.png 600w, /images/homekitchen-800.png 800w"
+        srcSet="/images/homefood-400.jpg 400w, /images/homefood-600.jpg 600w, /images/homefood-800.jpg 800w"
         alt="homepage"
       />
+    
       <h1 className={styles.homeTitle}>Welcome to MyRecipes!!!</h1>
       <p className={styles.homeText}>
         We have all recipes you need! 
