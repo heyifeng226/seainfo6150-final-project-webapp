0f13f203f457a63033d2129761be79c911881aa6
heyifeng226 <46611375+heyifeng226@users.noreply.github.com>
2020-12-07 02:37:25 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
-
+import recipes from "./data/recipes.json";
 import Home from "./Home/Home.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Nav from "./Nav/Nav.jsx";
-import Bar from "./Bar/Bar.jsx";
+//import Bar from "./Bar/Bar.jsx";
 import Error from "./Error/Error.jsx";
 //import styles from "./nav.module.css";
 import AboutUs from "./About/AboutUs.jsx";
 import AllCategory from "./AllCategory/AllCategory.jsx";
 import Category from "./Category/Category.jsx";
+import Detail from "./Detail/Detail.jsx";
 import Register from "./Register/Register.jsx";
 import Login from "./Login/Login.jsx";
 
@@ -20,6 +21,14 @@ const externalContent = {
   title: "An Article",
   author: "April Bingham",
   text: "Some text in the article",
+};
+
+const allCategorys = {
+  Soup: { id: "1", name: "Soup" },
+  Appetizer: { id: "2", name: "Appetizer" },
+  MainCourse: { id: "3", name: "Main Course" },
+  Snack: { id: "4", name: "Snack" },
+  Dessert: { id: "5", name: "Dessert" },
 };
 
 function App() {
@@ -37,18 +46,33 @@ function App() {
         <Route path="/category" exact component={AllCategory} />
         
         {/* passing parameters via a route path */}
+        
         <Route
-          path="/bar/:categoryId/:productId"
+          path="/category/:categoryID"
+          exact
+          render={({ match }) => (
+            <Category
+              categoryID={match.params.categoryID}
+              categorys={Object.values(allCategorys)}
+              content={Object.values(recipes)}
+            />
+          )}
+        />
+        <Route
+          path="/category/:categoryID/:recipeID"
           exact
           render={({ match }) => (
             // getting the parameters from the url and passing
             // down to the component as props
-            <Bar
-              categoryId={match.params.categoryId}
-              productId={match.params.productId}
+            <Detail
+              categoryID={match.params.categoryID}
+              recipeID={match.params.recipeID}
+              content={Object.values(recipes)}
             />
           )}
         />
+      
+        
         <Route path="/about" exact component={AboutUs} />
         
         <Route component={Error} />
---
+++ src/Detail/Detail.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Detail.module.css";
+
+
+const getRecipeDetail = (recipe, categoryID, recipeID) => {
+  if (recipe.categoryID === categoryID && recipe.recipeID === recipeID) {
+    return (
+      <div className={styles.recipeContainer}>
+        <img
+          className={styles.recipeImg}
+          src={recipe.imageURL}
+          alt={recipe.name}
+        />
+        <h1 className={styles.recipeName}>{recipe.name}</h1>
+        <h2 className={styles.recipeTitle}>Ingredients</h2>
+        <p className={styles.recipeIngredients}>{recipe.ingredients}</p>
+        <h2 className={styles.recipeTitle}>Cooking Directions</h2>
+        <p className={styles.recipeDetail}>Step1: {recipe.details.one}</p>
+        <p className={styles.recipeDetail}>Step2: {recipe.details.two}</p>
+        <p className={styles.recipeDetail}>Step3: {recipe.details.three}</p>
+        <p className={styles.recipeDetail}>Step4: {recipe.details.four}</p>
+        
+        <p>
+          <Link className={styles.recipeBackLink} to={recipe.categoryURL}>
+            &lt; Back to {recipe.categoryName} Page
+          </Link>
+        </p>
+      </div>
+    );
+  }
+};
+
+const Detail = (props) => {
+  return (
+    <div>
+      {props.content.map((recipe) =>
+        getRecipeDetail(recipe, props.categoryID, props.recipeID)
+      )}
+    </div>
+
+  );
+};
+
+export default Detail;
